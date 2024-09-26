d3e09a8c7c458008a460d880be21633221e47565
Ryo Novoa <ryosnovoa@gmail.com>
2024-09-26 09:31:36 -0700

    added contextmenus

---
+++ croc-thesaurus/background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "examineWord",
+      title: "Examine '%s'",
+      contexts: ["selection"]
+    });
+  });
+  
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "examineWord") {
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: fetchWordData,
+        args: [info.selectionText]
+      });
+    }
+  });
